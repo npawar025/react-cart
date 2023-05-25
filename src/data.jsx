7ac7464c24130189a45545f99bf8a157a53eddea
Nilesh <npawar025@gmail.com>
2023-05-25 20:38:16 +0530

    Initial commit

---
+++ src/data.jsx
@@ -0,0 +1,32 @@
+const cartItems = [
+  {
+    id: 'rec1JZlfCIBOPdcT2',
+    title: 'Samsung Galaxy S8',
+    price: '130',
+    img: 'https://images2.imgbox.com/c2/14/zedmXgs6_o.png',
+    amount: 1,
+  },
+  {
+    id: 'recB6qcHPxb62YJ75',
+    title: 'google pixel',
+    price: '290',
+    img: 'https://images2.imgbox.com/fb/3d/O4TPmhlt_o.png',
+    amount: 1,
+  },
+  {
+    id: 'recdRxBsE14Rr2VuJ',
+    title: 'Xiaomi Redmi Note 2',
+    price: '100',
+    img: 'https://images2.imgbox.com/4f/3d/WN3GvciF_o.png',
+    amount: 1,
+  },
+  {
+    id: 'recwTo160XST3PIoW',
+    title: 'Samsung Galaxy S7',
+    price: '166',
+    img: 'https://images2.imgbox.com/2e/7c/yFsJ4Zkb_o.png',
+    amount: 1,
+  },
+];
+
+export default cartItems;
